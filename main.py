df8460a27c6fdf8d78e2467791d7dc513ff247ec
MaksymChiachakov <maksymchiachakov@gmail.com>
2023-09-24 15:12:03 +0200

    update sql

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ cursor = conn.cursor()
 
 # Посилаємо -запит у DB
 # score - потрібно нам для безпечного надсилання запитів
-cursor.execute('''SELECT game_name FROM games''')
+cursor.execute('''SELECT * FROM games''')
 
 # Перетворення даних у список
 data = cursor.fetchall()
@@ -19,9 +19,8 @@ random_game = random.choice(data)
 print('Сьогодні пропонуємо вас зіграти у цю гру:', random_game)
 
 # Виводимо рандомний елемент з нашого списку даних. Version 2:
-random_index = random.randint(0, len(data))
-random_element = data[random_index]
-print('Сьогодні пропонуємо вас зіграти у цю гру:', random_element)
+random_index = random.randint(0, len(data) - 1)
+print('Сьогодні пропонуємо вас зіграти у цю гру:', data[random_index])
 
 # Виключаємо підключення до нашої DB, після цього, ми не зможемо надсилати запити до неї
 conn.commit()
